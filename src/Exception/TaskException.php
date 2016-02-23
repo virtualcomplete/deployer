a00d451cacb7584e831278a7a66a45a34450f15c
devonblzx <devon.b@virtualcomplete.com>
2016-02-23 17:17:22 -0500

    PSR-2 format
    Was using default phpstorm format for this project apparently.

--- src/Exception/TaskException.php
+++ src/Exception/TaskException.php
@@ -1,17 +1,20 @@
 <?php
 namespace Deployer\Exception;
 
-class TaskException extends \Exception {
+class TaskException extends \Exception
+{
 
-	public $stderr;
-	public $stdout;
+    public $stderr;
+    public $stdout;
 
-	public function getStderr() {
-		return $this->stderr;
-	}
+    public function getStderr()
+    {
+        return $this->stderr;
+    }
 
-	public function getStdout() {
-		return $this->stdout;
-	}
+    public function getStdout()
+    {
+        return $this->stdout;
+    }
 
 }
